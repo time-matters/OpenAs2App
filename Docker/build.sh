a1437faf49f11a7103f23fb3ec9f953d41c61442
Jon Rios <jrl@codecare.de>
2020-07-07 17:18:57 +0200

    simplify build process

--- Docker/build.sh
+++ Docker/build.sh
@@ -2,20 +2,7 @@
 
 # build server in parent dir
 cd ..
+
 mvn install -Dmaven.test.skip=true
-mv Server/dist/OpenAS2Server-2.10.0.zip Docker
-if [ -f "../aws/credentials" ]; then
-  cp ../aws/credentials Docker
-fi
-if [ -f "../config.xml" ]; then
-  cp ../config.xml Docker
-fi
 
-# return to this directory
-cd Docker
-
-docker build -t openas2-server:1.0.0 .
-
-rm *.zip
-rm credentials
-rm config.xml
+docker build -f Docker/Dockerfile -t openas2-server:1.0.0 .
