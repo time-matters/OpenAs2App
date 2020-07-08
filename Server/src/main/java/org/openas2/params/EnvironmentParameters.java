5c68e4e8767807cf29aca559cc057467e58936ea
Jon Rios <jrl@codecare.de>
2020-07-08 09:45:24 +0200

    correct code format

--- Server/src/main/java/org/openas2/params/EnvironmentParameters.java
+++ Server/src/main/java/org/openas2/params/EnvironmentParameters.java
@@ -1,25 +1,18 @@
 package org.openas2.params;
 
-public class EnvironmentParameters extends ParameterParser
-{
-    public EnvironmentParameters()
-    {
+public class EnvironmentParameters extends ParameterParser {
+    public EnvironmentParameters() {
         super();
     }
 
-    public void setParameter(String key, String value) throws InvalidParameterException
-    {
+    public void setParameter(String key, String value) throws InvalidParameterException {
         throw new InvalidParameterException("Set not supported", this, key, value);
     }
 
-    public String getParameter(String key) throws InvalidParameterException
-    {
-        if (key != null)
-        {
+    public String getParameter(String key) throws InvalidParameterException {
+        if (key != null) {
             return System.getenv(key);
-        }
-        else
-        {
+        } else {
             throw new InvalidParameterException("Invalid area in key", this, key, null);
         }
     }
